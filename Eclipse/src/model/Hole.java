32109d1e0d0480ee53ec1f96788f7dce61699cc3
Brand28 <alejandrobrand28@gmail.com>
2021-10-15 10:44:48 -0500

    imagenes y clase agjero

---
+++ Eclipse/src/model/Hole.java
@@ -0,0 +1,18 @@
+package model;
+
+import processing.core.PApplet;
+
+public class Hole extends Element{
+
+	public Hole(PApplet app, String iconAddress, int posX, int posY) {
+		super(app, iconAddress, posX, posY);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
